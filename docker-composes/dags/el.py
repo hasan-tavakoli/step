df9e703048262153722b981d7e8158508f2f1474
hasan.tavakoli1369@gmail.com <hasan.tavakoli1369@gmail.com>
2022-06-26 20:39:22 +0000

    run spark app

--- docker-composes/dags/el.py
+++ docker-composes/dags/el.py
@@ -2,25 +2,24 @@ import airflow
 from datetime import datetime, timedelta
 from airflow.operators.python_operator import PythonOperator
 from pyspark.sql import SparkSession, functions
+from datetime import datetime, timedelta
+now = datetime.now()
 
 def processo_etl_spark():
     spark = SparkSession \
         .builder \
-        .appName("Extração Documentos a Pagar") \
+        .appName("test") \
         .config("spark.jars.packages",
                 "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1") \
         .config("spark.mongodb.input.uri", "mongodb://mongo1:27017/mydb.user?readPreference=primaryPreferred") \
         .config("spark.mongodb.output.uri", "mongodb://mongo1:27017/mydb.user?readPreference=primaryPreferred") \
-        .config("spark.driver.maxResultSize", "4g") \
-        .config("spark.network.timeout", 10000000) \
-        .config("spark.executor.heartbeatInterval", 10000000) \
-        .config("spark.storage.blockManagerSlaveTimeoutMs", 10000000) \
-        .config("spark.executor.memory", "2g") \
         .master("spark://spark:7077") \
         .getOrCreate()
 
     data = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("spark.mongodb.input.uri","mongodb://mongo1:27017/mydb.step?authSource=admin").load()
-    print(data.count())
+    c=data.count()
+    print("count=")
+    print(c)
 
 default_args = {
     "owner": "airflow",
