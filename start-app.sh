e8a65f6b1d5d6854604af22bfa85e66b1ed2f531
Hassan Tavakoli <51287865+hasan-tavakoli@users.noreply.github.com>
2022-06-27 12:37:52 +0430

    Update start-app.sh

--- start-app.sh
+++ start-app.sh
@@ -1,16 +1,16 @@
 docker network create ranger-env
 
-cd docker-composes/ranger
-docker-compose up -d --build
+# cd docker-composes/ranger
+# docker-compose up -d --build
 
-cd ../hadoop
+cd docker-composes/hadoop
 docker-compose up -d --build
 
 cd ../mongo
 docker-compose up -d --build
 
-# cd ../docker/docker-airflow
-# docker build --rm --force-rm -t docker-airflow-spark:1.10.7_3.1.2 .
+cd ../docker/docker-airflow
+docker build --rm --force-rm -t docker-airflow-spark:1.10.7_3.1.2 .
 
 cd ../docker
 docker-compose up -d --build
