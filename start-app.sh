24b2f9d6f8ecba2be75a0843fe9ef33efdb11503
Hassan Tavakoli <51287865+hasan-tavakoli@users.noreply.github.com>
2022-06-27 10:07:13 +0430

    Update start-app.sh

--- start-app.sh
+++ start-app.sh
@@ -6,3 +6,11 @@ docker-compose up -d --build
 cd ../hadoop
 docker-compose up -d --build
 
+cd ../mongo
+docker-compose up -d --build
+
+cd ../docker/docker-airflow
+docker build --rm --force-rm -t docker-airflow-spark:1.10.7_3.1.2 .
+
+cd ..
+docker-compose up -d --build
