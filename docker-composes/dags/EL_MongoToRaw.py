9cf2775fde963094bb19225678a4bcf766634221
hasan.tavakoli1369@gmail.com <hasan.tavakoli1369@gmail.com>
2022-06-28 20:56:22 +0000

    program EL

--- docker-composes/dags/EL_MongoToRaw.py
+++ docker-composes/dags/EL_MongoToRaw.py
@@ -1,12 +1,34 @@
 import airflow
 from datetime import datetime, timedelta
 from airflow.operators.python_operator import PythonOperator
-from pyspark.sql import SparkSession, functions
+import pyspark
+from pyspark.sql import SparkSession
+from airflow.models import DAG
 from datetime import datetime, timedelta
 now = datetime.now()
 
-def processo_etl_spark():
-    spark = SparkSession \
+
+args = {
+    'owner': 'Hassan',
+    "start_date": datetime(now.year, now.month, now.day),
+    'provide_context': True,
+    'depends_on_past': True,
+    'wait_for_downstream': True,
+}
+
+dag = DAG(
+    dag_id='EL_MongoToRaw',
+    default_args=args,
+    schedule_interval=timedelta(1),
+    max_active_runs=1,
+    concurrency=1
+)
+
+
+# -----------------------------------------------------------------------------------------
+# inner functions
+def create_spark_session(spark_host):
+    app = SparkSession \
         .builder \
         .appName("test") \
         .config("spark.jars.packages",
@@ -15,25 +37,50 @@ def processo_etl_spark():
         .config("spark.mongodb.output.uri", "mongodb://mongo1:27017/mydb.user?readPreference=primaryPreferred") \
         .master("spark://spark:7077") \
         .getOrCreate()
+    return app
 
-    data = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("spark.mongodb.input.uri","mongodb://mongo1:27017/mydb.step?authSource=admin").load()
-    data .write.format("parquet").save("hdfs://namenode:9000/EDL/steps.parquet")
-    print("finish")
+def read_db(url, spark_app):
+    read_connection = spark_app.read.format("com.mongodb.spark.sql.DefaultSource")
+    add_url = read_connection.option("spark.mongodb.input.uri", url)
+    read_data = add_url.load()
+    return read_data
 
-default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "start_date": datetime(now.year, now.month, now.day),
-    "email": ["airflow@airflow.com"],
-    "email_on_failure": False,
-    "email_on_retry": False,
-    "retries": 1,
-    "retry_delay": timedelta(minutes=1)
-}
-with airflow.DAG('EL_MongoToRaw',
-                  default_args=default_args,
-                  schedule_interval=timedelta(1)) as dag:
-    task_elt_documento_pagar = PythonOperator(
-        task_id='elt_documento_pagar_spark',
-        python_callable=processo_etl_spark
-    )
+
+# -----------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------
+
+# task functions
+
+# -----------------------------------------------------------------------------------------
+def compress_to_raw(**kwargs):
+    url = "mongodb://mongo1:27017/mydb.step?authSource=admin"
+    # read data from MongoDB
+    spark = create_spark_session("spark.mongodb.input.uri")
+    data = read_db(url, spark)
+    if (data != None):
+        print("loaded data from MongoDB successfully.")
+    else:
+        print("check the data in MongoDB. It does'nt exist")
+    # compress and partition data by partition_col value
+    data.write.format("parquet").save("hdfs://namenode:9000//EDL_Data/Raw_Data_Zone/steps.parquet")
+
+    # data\
+    #     .withColumn("year", f.year(from_unixtime(f.col(partition_col)/1000).cast("timestamp")))\
+    #     .withColumn("month", f.month(from_unixtime(f.col(partition_col)/1000).cast("timestamp")))\
+    #     .withColumn("day", f.dayofmonth(from_unixtime(f.col(partition_col)/1000).cast("timestamp")))\
+    #     .write\
+    #     .partitionBy("year", "month", "day")\
+    #     .mode("overwrite")\
+    #     .option("compression", "snappy")\
+    #     .parquet(raw_path)
+
+
+# -----------------------------------------------------------------------------------------
+# operators
+t_compress_to_raw = PythonOperator(
+    task_id='compress_to_raw',
+    python_callable=compress_to_raw,
+    dag=dag, )
+
+t_compress_to_raw
