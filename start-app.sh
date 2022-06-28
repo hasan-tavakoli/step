083203c976310b8e73e36b920dd88d087fd9d78c
hasan.tavakoli1369@gmail.com <hasan.tavakoli1369@gmail.com>
2022-06-28 13:09:52 +0000

    config ranger

--- start-app.sh
+++ start-app.sh
@@ -1,16 +1,16 @@
 docker network create ranger-env
 
-# cd docker-composes/ranger
-# docker-compose up -d --build
-
-cd docker-composes/hadoop
+cd docker-composes/ranger
 docker-compose up -d --build
 
-cd ../mongo
+cd ../hadoop
 docker-compose up -d --build
 
-cd ../docker/docker-airflow
-docker build --rm --force-rm -t docker-airflow-spark:1.10.7_3.1.2 .
+#cd ../mongo
+#docker-compose up -d --build
 
-cd ../docker
-docker-compose up -d --build
+#cd ../docker/docker-airflow
+#docker build --rm --force-rm -t docker-airflow-spark:1.10.7_3.1.2 .
+
+#cd ../docker
+#docker-compose up -d --build
