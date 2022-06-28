40b277f44f4d7859f04a9488b9227db5fd7a8a17
hasan.tavakoli1369@gmail.com <hasan.tavakoli1369@gmail.com>
2022-06-28 13:23:44 +0000

    zip data ranger

--- stop-app.sh
+++ stop-app.sh
@@ -1,3 +1,7 @@
-docker stop $(docker ps -aq)
-docker rm $(docker ps -aq)
-docker rmi $(docker images -aq)
+#docker stop $(docker ps -aq)
+#docker rm $(docker ps -aq)
+#docker rmi $(docker images -aq)
+cd docker-composes/ranger/
+zip -r  db_data.zip db_data/
+sudo rm -r  db_data/
+
