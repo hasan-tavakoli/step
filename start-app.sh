a4151d2d9a917bfaa2881181083d496a39525f25
hasan.tavakoli1369@gmail.com <hasan.tavakoli1369@gmail.com>
2022-06-30 13:07:49 +0000

    setup metabase & config ranger

--- start-app.sh
+++ start-app.sh
@@ -14,11 +14,11 @@ unzip hadoop_namenode.zip
 rm hadoop_namenode.zip
 docker-compose up -d --build
 
-cd ../mongo
-docker-compose up -d --build
+#cd ../mongo
+#docker-compose up -d --build
 
-cd ../docker/docker-airflow
-docker build --rm --force-rm -t docker-airflow-spark:1.10.7_3.1.2 .
+#cd ../docker/docker-airflow
+#docker build --rm --force-rm -t docker-airflow-spark:1.10.7_3.1.2 .
 
 cd ../docker
 docker-compose up -d --build
